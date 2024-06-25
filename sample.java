433cb44650ac7cd8a7636da0055f094d394f21c4
Deepak Jaya Priyan <dhonideepak672@gmail.com>
2024-06-25 16:34:08 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class sample{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the elements");
+
+    int n=sc.nextInt();
+    String a= Integer.toString(n);
+    
+        System.out.println(a.length());
+    }
+    
+    
+
+    
+
+    
+        
+    }
+    
+
